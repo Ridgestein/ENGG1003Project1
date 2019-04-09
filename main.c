823c8749d0658ff161781f3a211c6269dcedc49a
Daniel  Waite <whitepanda64@gmail.com>
2019-04-09 02:38:48 +0000

    Added the ability to read/write to and from a txt file

--- main.c
+++ main.c
@@ -2,26 +2,38 @@
 
 char RoC (char m, char c, char message); // function defintion for rotation cipher
 
+
+
 int main()
 {
-    int k = 0 ; // the cipher key
-    char m; // a letter of the message before encryption
-    char c ; // a letter of the message after encryption
-    char message[100] = "Hello There"; // the given message to be encrypted
-    //scanf("%s", message);
     
-    return k;
-}
-
-char RoC (char m, char c, char message) {
-    //for loop here later for rotation
-    int k = 0;
+    FILE *input;
+    FILE *output;
     
-    for(k = 0; k <= 26; k++;) {
-        message = message (m + k % 26);
-        printf("%s", &message);
+    input = fopen("input.txt", "r");
+    output = fopen("output.txt", "w");
+    
+    char c; //a single letter of the message to be read
+    while(fscanf(input, "%c", &c) == 1){
+            
+        char RoC (char c) {
+    
+        int k = 0; //the current cipher key
+        for(int k = 0; k <= 26; k++) {
+         c = (c - k % 26);
+         //printf("%c", &c);
+        
     } 
     
     
-    return message;
+    return k;
+
+    }
+        fprintf(output, "%c", c);
+          
+    }
+
+   // printf("\n");
+    
+
 }
