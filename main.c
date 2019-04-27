1e44ada32ee938173cb33b308b836a3f7dcbf5e5
Daniel  Waite <whitepanda64@gmail.com>
2019-04-27 06:35:09 +0000

    Began to implement a subsitution cipher.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h> 
 #include <stdlib.h>
-char RoCE (char *message, int k, int length); //function defintion for rotation cipher encryption
-char RoCD (char *message, int k, int length); //function defintion for rotation cihper decryption
-//char SUBCE (char *message, int k, int length); //the function definiton for substitution cipher encryption
+char ROCE (char *message, int k, int length); //function defintion for rotation cipher encryption
+char ROCD (char *message, int k, int length); //function defintion for rotation cihper decryption
+char SUBCE (char *message, char *subK, int k, int length); //the function definiton for substitution cipher encryption
 //char SUBCD
 
 
@@ -15,10 +15,11 @@ int main()
    // input = fopen("input.txt", "r");
    // output = fopen("output.txt", "w");
     
-    char message[] = "HELLO THERE"; //hardcoded message
+    char message[] = "HELLO THERE"; //hardcoded message to be translated 
     int k = 10; //the current cipher key from 1 to 25
-    int l = sizeof(message)/sizeof(message[0]); //allows the changing size of the message array to be updated when the message is changed gu
-    int option; 
+    char subK[] = "PHQGIUMEAYLNOFDXJKRCVSTZWB"; //the key specifically for the substitution cipher  
+    int l = sizeof(message)/sizeof(message[0]); //allows the changing size of the message array to be updated when the message is changed
+    int option; //allows for the user interface to be interacted with 
     //while(fscanf(input, "%c", &c) == 1){
         
     //printf("Enter a message to encrypt: ");
@@ -31,17 +32,16 @@ do {
     switch (option) { 
     
         case 1:
-            RoCE(message, k, l);
+            ROCE(message, k, l);
             break;  
         
         case 2:
             for(k = 0; k < 26; k++) {
-                RoCD(message, k, l); 
-                break;
-            }    
-            
+                ROCD(message, k, l); 
+            }   
+            break;
     }
-   //message[i] = ch;
+   
    //fprintf(output, "%c", c);
         
    
@@ -53,47 +53,74 @@ return 0;
     
     
     //printf("\n");
-char RoCE (char *message, int k, int length) {
+char ROCE (char *message, int k, int length) {
              
         int i; //the current postion of a indiviual character being read
         char ch; //single character of the message to be read
-        char result[length]; 
+        char result[length]; //variable for the final encrypted/decrypted message to be stored in
         
         for(i = 0; message[i] != '\0'; ++i){
             ch = message[i];
-            if (message[i] >= 65 && message[i] <= 90) { //checks for non-alphabet characters like the space character
+            if (message[i] >= 65 && message[i] <= 90) { //checks for non-alphabet characters e.g. the space character
                 ch = (ch + k - 65) % 26 + 65; 
             }
         result[i] = ch;
         }
-        result[length] = '\0';
+       // result[length] = '\0';
             
         printf("Encrypted message: %s\n", result);
 
    return 0; 
 }
 
-char RoCD (char *message, int k, int length) {
+char ROCD (char *message, int k, int length) {
     
-    int i; //the current postion of a indiviual character being read
-    char ch; //single character of the message to be read in 
+    int i; 
+    char ch; 
     char result[length];
     int e_count = 0;
     
     for(i = 0; message[i] != '\0'; ++i){
         ch = message[i];
-        if (message[i] >= 65 && message[i] <= 90) { //checks for non-alphabet characters like the space character
+        if (message[i] >= 65 && message[i] <= 90) { 
             ch = (ch - k - 65 + 26) % 26 + 65;
         }
-        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            e_count++;
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') { //checks for vowels and adds them to a counter, making it easier to tell if a decrypted message is the right one
+            e_count++;  
         }
         result[i] = ch;
-        }
-        result[length] = '\0';
+    }
+        //result[length] = '\0';
             
-        printf("e's: %d, Decrypted message: %s\n", e_count, result);
+        printf("Vowels: %d, Decrypted message: %s\n\n", e_count, result);
     
     return 0;
     
+}
+
+char SUBCE (char *message, char *subK, int k, int length) {
+    
+    int i; 
+    int x; //the current position of 
+    char ch; 
+    char result[length]; 
+    //char subK[] = "PHQGIUMEAYLNOFDXJKRCVSTZWB";
+    char nAlpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXZY";
+    
+    for(i = 0; message[i] != '\0'; ++i){
+        ch = message[i];
+        if (nAlpha[i] != ch) {
+            nAlpha[i] = subK[i];
+        }
+
+        if (message[i] >= 65 && message[i] <= 90) { 
+           // ch = (ch + k - 65) % 26 + 65; 
+        }   
+        result[i] = ch;
+    }
+        //result[length] = '\0';
+        
+        printf("Encrypted message: %s\n", result);
+        
+    return 0;
 }
