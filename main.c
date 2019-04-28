40db6b060008d400827cc8f461cdd87eaca43d27
Daniel  Waite <whitepanda64@gmail.com>
2019-04-28 12:40:48 +0000

    Implemented substitution cipher encryption though could not find a way to ignore other characters that didn't cause errors (like segmentation faults), but it works effectively without spaces in the message variable
    Also, for a while my program has been randomly crashing when the compile button is clicked, sometimes it happens but sometimes it doesn't. Same goes for using './a.out' in the terminal which sometimes causes a seg fault. Hopefully I can clear up the issue in my lab, otherwise marking may be annoying.

--- main.c
+++ main.c
@@ -1,33 +1,21 @@
 #include <stdio.h> 
 #include <stdlib.h>
+
 char ROCE (char *message, int k, int length); //function defintion for rotation cipher encryption
 char ROCD (char *message, int k, int length); //function defintion for rotation cihper decryption
-char SUBCE (char *message, char *subK, int k, int length); //the function definiton for substitution cipher encryption
-//char SUBCD
-
+char SUBCE (char *message, char *subK, int length); //the function definiton for substitution cipher encryption
 
 int main()
 {
-    
-    //FILE *input;
-    //FILE *output;
-    
-   // input = fopen("input.txt", "r");
-   // output = fopen("output.txt", "w");
-    
-    char message[] = "HELLO THERE"; //hardcoded message to be translated 
+
+    char message[20] = "HELLO THERE"; //hardcoded message to be translated 
     int k = 10; //the current cipher key from 1 to 25
-    char subK[] = "PHQGIUMEAYLNOFDXJKRCVSTZWB"; //the key specifically for the substitution cipher  
+    char subK[30] = "PHQGIUMEAYLNOFDXJKRCVSTZWB"; //the key specifically for the substitution cipher  
     int l = sizeof(message)/sizeof(message[0]); //allows the changing size of the message array to be updated when the message is changed
     int option; //allows for the user interface to be interacted with 
-    //while(fscanf(input, "%c", &c) == 1){
-        
-    //printf("Enter a message to encrypt: ");
-
-    //scanf("%s", message);
             
 do {
-    printf("Select one: Rotation Encrypt(1), Rotation Decrypt(2), Exit(0)");
+    printf("Select one: Rotation Encrypt(1), Rotation Decrypt(2), Substitution Encrypt(3), Exit(0)"); //prints the user interface, which is then interacted with by the 'option variable'
     scanf("%d", &option); 
     switch (option) { 
     
@@ -40,19 +28,17 @@ do {
                 ROCD(message, k, l); 
             }   
             break;
+        
+        case 3:
+            SUBCE(message, subK, l);
+            break;
     }
    
-   //fprintf(output, "%c", c);
-        
-   
-    
     } while (option != 0);
 
 return 0;
 }
     
-    
-    //printf("\n");
 char ROCE (char *message, int k, int length) {
              
         int i; //the current postion of a indiviual character being read
@@ -66,7 +52,7 @@ char ROCE (char *message, int k, int length) {
             }
         result[i] = ch;
         }
-       // result[length] = '\0';
+        result[length] = '\0';
             
         printf("Encrypted message: %s\n", result);
 
@@ -90,7 +76,7 @@ char ROCD (char *message, int k, int length) {
         }
         result[i] = ch;
     }
-        //result[length] = '\0';
+        result[length] = '\0';
             
         printf("Vowels: %d, Decrypted message: %s\n\n", e_count, result);
     
@@ -98,28 +84,26 @@ char ROCD (char *message, int k, int length) {
     
 }
 
-char SUBCE (char *message, char *subK, int k, int length) {
+char SUBCE (char *message, char *subK, int length) {
     
-    int i; 
-    int x; //the current position of 
-    char ch; 
+    int i = 0; 
+    int x = 0; //the current position of a letter in the nAlpha and subK arrays
+    //char ch; 
     char result[length]; 
     //char subK[] = "PHQGIUMEAYLNOFDXJKRCVSTZWB";
-    char nAlpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXZY";
+    char nAlpha[30] = "ABCDEFGHIJKLMNOPQRSTUVWXZY"; //the normal alphabet to be compared to the cipher alphabet
     
-    for(i = 0; message[i] != '\0'; ++i){
-        ch = message[i];
-        if (nAlpha[i] != ch) {
-            nAlpha[i] = subK[i];
+    while (message[i] != '\0') {
+        if (nAlpha[x] == message[i]) {
+        result[i] = subK[x];
+        i++;
+        x = 0; 
         }
-
-        if (message[i] >= 65 && message[i] <= 90) { 
-           // ch = (ch + k - 65) % 26 + 65; 
-        }   
-        result[i] = ch;
+        else {
+        x++;    
+        }
     }
-        //result[length] = '\0';
-        
+
         printf("Encrypted message: %s\n", result);
         
     return 0;
